60a80ea314c9ec35b90d4611536d2e24a827242b
Avinash Prabhakaran <zi.avi7@gmail.com>
2023-10-01 16:39:34 -0400

    Update navbar and links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,11 +4,16 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <div className='h-12 bg-dark text-gold flex justify-end text-lg font-bold'>
-        <Link className='mr-auto p-3' href="#">P.K Balakrishnan</Link>
-        <Link className='p-3' href="#">Buy Books</Link>
-        <Link className='p-3' href="#">About</Link>
-        <Link className='p-3' href="#">Books</Link>
-        <Link className='p-3' href="#">Gallery</Link>
+        <Link className='mr-auto p-3' href="/">P.K Balakrishnan</Link>
+        <Link className='p-3'
+            target="_blank"
+            href="https://dcbookstore.com/authors/p.-k.-balakrishnan"
+        >
+          Buy Books
+        </Link>
+        <Link className='p-3' href="/about">About</Link>
+        <Link className='p-3' href="/books">Books</Link>
+        <Link className='p-3' href="/gallery">Gallery</Link>
     </div>
   )
 }
