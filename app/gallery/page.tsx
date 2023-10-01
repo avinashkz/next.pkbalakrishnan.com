367c4a14e81364c9f06e3c84535bf92c8584c512
Avinash Prabhakaran <zi.avi7@gmail.com>
2023-10-01 16:43:06 -0400

    Stage changes for gallery

---
+++ app/gallery/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div className='bg-light-gold'>
+        <h1 className='heading-1'>1940s</h1>
+        <h1 className='heading-1'>1950s</h1>
+        <h1 className='heading-1'>1960s</h1>
+        <h1 className='heading-1'>1970s</h1>
+        <h1 className='heading-1'>1980s</h1>
+    </div>
+  )
+}
+
+export default Gallery
