489d0598e7181cef761938a88d84cf785e3d0220
Avinash Prabhakaran <zi.avi7@gmail.com>
2023-10-04 22:53:44 -0400

    Test husky

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
-import './globals.css';
+import './globals.css'
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/navbar';
