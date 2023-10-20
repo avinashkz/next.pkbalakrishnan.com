27868e7df3347ee25e419ea1816ebb0e9adcd125
Avinash Prabhakaran <zi.avi7@gmail.com>
2023-10-19 21:06:15 -0400

    Update navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,30 +1,102 @@
-import React from 'react';
-import Link from 'next/link';
+'use client';
 
-function Navbar() {
-  return (
-    <div className="flex h-12 justify-end bg-dark text-lg font-bold text-gold">
-      <Link className="mr-auto min-w-fit p-3" href="/">
-        P.K Balakrishnan
-      </Link>
-      <Link
-        className="min-w-fit p-3"
-        target="_blank"
-        href="https://dcbookstore.com/authors/p.-k.-balakrishnan"
-      >
-        Buy Books
-      </Link>
-      <Link className="p-3" href="/about">
-        About
-      </Link>
-      <Link className="p-3" href="/books">
-        Books
-      </Link>
-      <Link className="p-3" href="/gallery">
-        Gallery
-      </Link>
-    </div>
-  );
+import Link from 'next/link';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+const navigation = [
+  { name: 'About', href: '/about', current: true },
+  { name: 'Books', href: '/books', current: false },
+  { name: 'Gallery', href: '/gallery', current: false },
+  { name: 'Contact', href: '/contact', current: false },
+];
+
+function classNames(...classes: Array<string>) {
+  return classes.filter(Boolean).join(' ');
 }
 
-export default Navbar;
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-dark">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-14 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <span className="h-auto w-auto font-bold text-gold">
+                    P.K Balakrishnan
+                  </span>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-gold text-white'
+                            : 'text-gold hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-bold',
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Link
+                  target="_blank"
+                  href="https://dcbookstore.com/authors/p.-k.-balakrishnan"
+                  className="relative p-1 font-bold text-gray-400"
+                >
+                  {/* <span className="absolute -inset-1.5" /> */}
+                  <span className="h-6 w-6 text-gold hover:text-white">
+                    Buy Books
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'cursor-auto bg-dark text-white'
+                      : 'text-gold hover:bg-gold hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
